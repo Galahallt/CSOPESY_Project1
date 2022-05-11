b33f3fe14f2e5a743ef664fb1f6890084c52b0a6
Andre Ponce <75112557+su0h@users.noreply.github.com>
2022-05-11 12:17:44 +0800

    Added Round Robin implementation
    Added Round Robin implementation
    Added switch-case in main function

--- ESPIRITU_PONCE_1.c
+++ ESPIRITU_PONCE_1.c
@@ -11,8 +11,12 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TRUE 1
+#define FALSE 0
 
 typedef struct ExecTime {
   int start;
@@ -28,7 +32,8 @@ typedef struct Process {
   ExecTime* execTimes;
 } Process;
 
-Process* FCFS(Process* processes, int size) {
+// Performs the First-Come First-Served Scheduling Algorithm.
+Process* performFCFS(Process* processes, int size) {
   Process* result = malloc(sizeof(Process) * size); // allocate memory for result of process
   
   int ctr = 0;  // keep track of the number of processes
@@ -75,7 +80,8 @@ Process* FCFS(Process* processes, int size) {
   return result; // return the result which is an array of updated processes 
 }
 
-Process* SJF(Process* processes, int size) {
+// Performs the Shortest Job First Scheduling Algorithm.
+Process* performSJF(Process* processes, int size) {
   Process* result = malloc(sizeof(Process) * size); // allocate memory for result of process
   
   int ctr = 0;  // keep track of the number of processes
@@ -130,6 +136,95 @@ Process* SJF(Process* processes, int size) {
   return result; // return the result which is an array of updated processes 
 }
 
+// Performs the Round Robin Scheduling Algorithm.
+Process* performRR (Process* processes, int processAmt, int quantum) {
+  // array containing the results
+  Process* result = malloc(sizeof(Process) * processAmt);
+  // counter for the processes that finished executing
+  int processCtr = 0;
+  // current time of execution
+  int currTime = 0;
+  // index of process being executed
+  int index = 0;
+
+  while (processCtr < processAmt) {
+    // get current process
+    Process p = processes[index];
+
+    // if process has arrived and is still not finished executing
+    if (p.arrivalTime <= currTime && p.burstTime > 0) {
+      // allocate for start-end times
+      p.execTimes = realloc(p.execTimes, sizeof(ExecTime) * (p.execTimesLength + 1));
+
+      // compute for waiting time value to be added
+      int waitingTimeVal = (p.burstTime >= quantum ? quantum : p.burstTime);
+
+      // set start time
+      p.execTimes[p.execTimesLength].start = currTime;
+      // set end time
+      p.execTimes[p.execTimesLength].end = currTime + waitingTimeVal;
+
+      // increase wait times of all other processes
+      for (int i = 0; i < processAmt; i++) {
+        /**
+          * If process is:
+          * - not the same as the executing process
+          * - not yet finished executing
+          * - has arrived already
+          * then increase its waiting time
+          */
+        if (processes[i].id != p.id && 
+            processes[i].burstTime > 0 && 
+            processes[i].arrivalTime <= currTime) {
+          processes[i].waitingTime += waitingTimeVal;
+        }
+      }
+
+      // decrease remaining time of process
+      p.burstTime -= waitingTimeVal;
+      // +1 in no. of times executed for this process
+      p.execTimesLength++;
+
+      // update process in input list
+      processes[index] = p;
+
+      // increase time by waiting time value
+      currTime += waitingTimeVal;
+      
+      // if process is finished executing
+      if (p.burstTime <= 0) {
+        // store process in result index
+        result[index] = p;
+        // increment no. of processes finished
+        processCtr++;
+      }
+    }
+
+    // if index exceeds bounds, reset to 0
+    if (index >= processAmt) {
+      int hasExecutableProcess = FALSE;
+      // traverse processes array and check for any executable processes
+      for (int i = 0; i < processAmt; i++) 
+        if (processes[i].arrivalTime <= currTime && processes[i].burstTime > 0) {
+          hasExecutableProcess = TRUE;
+          break;
+        }
+
+      // if there are no executable processes, advance time
+      if (!hasExecutableProcess) 
+        currTime++;
+
+      // reset index
+      index = 0;
+    }
+    // else, increment
+    else
+      index++;
+  }
+
+  return result;
+}
+
 int main() {
   char fName[128];
   FILE *file;
@@ -151,7 +246,7 @@ int main() {
     // Z denotes a time slice value (only for RR)
     int X, Y, Z;
     if (!(fscanf(file, " %d %d %d", &X, &Y, &Z))) {
-      printf("Invalid inputs, check contents of %s", file);
+      printf("Invalid inputs, check contents of %s", fName);
       exit(1);
     } else {
       // retrieve Y processes in text file
@@ -160,7 +255,7 @@ int main() {
       for (int i = 0; i < Y; i++) {
         // validate inputs of text file
         if (!(fscanf(file, " %d %d %d", &processID, &arrivalTime, &burstTime))) {
-          printf("Invalid inputs, check contents of %s", file);
+          printf("Invalid inputs, check contents of %s", fName);
           exit(1);
         } else {
           // store inputs from text file to process struct
@@ -176,22 +271,37 @@ int main() {
       // close file
       fclose(file);
 
-      // store result of First-Come-First-Serve algorithm to a Process struct
-      Process* resultFCFS = SJF(p, Y);
-
-      // display the results of the First-Come-First-Serve algorithm
-      for (int j = 0; j < Y; j++) {
-        printf("P[%d] ", resultFCFS[j].id);
-        for (int k = 0; k < resultFCFS[j].execTimesLength; k++) {
-          printf("Start Time: %d End Time: %d | ", (*(resultFCFS[j].execTimes + k)).start, (*(resultFCFS[j].execTimes + k)).end);
-        }
-        printf("Waiting time: %d\n", resultFCFS[j].waitingTime);
+      // store results here
+      Process* results;
+      // switch-case for various scheduling algorithms
+      switch (X) {
+        case 0:
+          results = performFCFS(p, Y);
+          break;
+        case 1:
+          results = performSJF(p, Y);
+          break;
+        case 2:
+          // results = performSRTF(p, Y);
+          break;
+        case 3:
+          results = performRR(p, Y, Z);
+          break;
       }
 
-      // compute the average waiting time of the First-Come-First-Serve algorithm
+      // print start time/s, end time/s, and waiting time
+      for (int j = 0; j < Y; j++) {
+        printf("P[%d] ", results[j].id);
+        for (int k = 0; k < results[j].execTimesLength; k++) {
+          printf("Start Time: %d End Time: %d | ", (*(results[j].execTimes + k)).start, (*(results[j].execTimes + k)).end);
+        }
+        printf("Waiting time: %d\n", results[j].waitingTime);
+      }
+
+      // compute the average waiting time of the scheduling algorithm used
       double avgTime = 0.0;
       for (int l = 0; l < Y; l++) {
-        avgTime += (double) resultFCFS[l].waitingTime;
+        avgTime += (double) results[l].waitingTime;
       }
       avgTime /= (double) Y;
       printf("Average waiting time: %.2f", avgTime);
